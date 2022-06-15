7691a253fd4d31fb966fb71cfd2f1723572f96de
ShreyasNandish <shreyasnandish4@gmail.com>
2022-06-15 08:45:12 +0530

    first ifelse syntax

---
+++ ifElse.sh
@@ -0,0 +1,26 @@
+#! /bin/bash -x
+
+isValue=true
+
+if [ #check condition ]
+then
+	#body
+fi
+
+#2)
+if [ #check condition]
+then 	#body of if
+else
+	#body of if
+fi
+
+#3)
+if [ #check condition ]
+then
+	#body of if
+elif [ #second checkcondition ]
+then
+	#body of else if
+else
+	#body of second else
+fi
